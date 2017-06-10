cf1ded47777d8c7b91faa60c7d0f8df557e3addc
aabeas <aaron.beasley@gmail.com>
2017-06-09 20:02:05 -0500

    JS Project Arrays and Loops
    Very excited about transcending this project.

---
+++ main.js
@@ -0,0 +1,195 @@
+// function signature = input/output
+  // pseudocode / write out steps logically before coding
+
+// 1.
+// Without logging the values, list the value of `sports` and `total`
+var sports = ['soccer', 'baseball'];
+var total = sports.push('football', 'swimming');
+// A:
+// sports = <['soccer', 'baseball']>
+// total = <['soccer', 'baseball', 'football', 'swimming']>
+
+// 2.
+// Using the `strings` array, wrte a function `longestString()`
+// that accepts an array argument and returns the longest string in the array
+let strings = ['this','is','a','collection','of','words'];
+// A:
+
+function longestString(strings) {
+  let longString = " ";
+  for (let i = 0; i < strings.length; i++){
+    if (strings[i].length > longString.length){
+        longString = strings[i]
+    }
+}	return longString;
+}
+
+ console.log(longestString(strings));
+
+
+console.assert(longestString(strings) === 'collection', {"message": "longestString should return 'collection'"});
+
+
+// Use the `numbers` array for questions 3 - 8.
+var numbers = [1,12,4,18,9,7,11,3,101,5,6];
+
+
+// 3.
+// Write a function `smallestNumber()` that accepts an array
+// and returns the smallest number in the array.
+// A: // kenji - min method is one option
+
+function smallestNumber(array) {
+  let smallNum = numbers[0];
+  for (let i = 0; i < array.length; i++){
+    if (array[i].length < smallNum.length){
+    smallNum = numbers[i];
+    }
+  }	return smallNum;
+}
+
+console.log(smallestNumber(numbers));
+
+console.assert(smallestNumber(numbers) === 1, {"message": "smallestNumber should return 1"});
+
+
+// 4.
+// Write a function `getEvens()` that accepts an array
+// and returns only the even numbers in the array.
+// A: // kenji - modulo
+
+function getEvens(array) {
+  let allEvens = [];
+  for (let i = 0; i < array.length; i++){
+    if ((array[i] % 2) === 0){
+        allEvens.push(array[i]);
+        }
+    }		return allEvens.toString();
+}
+
+console.log(getEvens(numbers));
+
+console.assert(getEvens(numbers).toString() === '12,4,18,6', {'message': 'getEvens should return "12,4,18,6"'});
+
+
+// 5.
+// Write a function that accepts an array argument
+// and returns the array reversed
+// Hint: When looping over the array, start at the last index
+// and decrement the iterator to zero
+// A: // kenji - reverse method
+
+function arrayReverser(array) {
+  let allReverse = array.reverse(); {
+  }	return allReverse.toString();
+}
+
+console.log(arrayReverser(numbers))
+
+console.assert(arrayReverser(numbers).toString() === '6,5,101,3,11,7,9,18,4,12,1', {'message': 'arrayReverser should return "6,5,101,3,11,7,9,18,4,12,1"'});
+
+
+// 6.
+// Write a function that accepts an array argument
+// and returns the sum of all of the numbers in the array
+// A: kenji - sum method
+
+function sumArrayOfNumbers(array) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++){
+  count += array[i];{
+    }
+  }		return count;
+}
+
+console.log(sumArrayOfNumbers(numbers));
+
+console.assert(sumArrayOfNumbers(numbers) === 177, {'message': 'sumArrayOfNumbers should return 177'});
+
+
+// 7.
+// Write a function that accepts an array argument
+// and returns an array of only the numbers greater than 10
+// A: kenji - comparison operator
+
+function numbersOver10(array) {
+  let overTen = [];
+  for (let i = 0; i < array.length; i++){
+    if ((array[i]) > 10){
+        overTen.push(array[i]);
+        }
+    }		return overTen;
+}
+
+console.log(numbersOver10(numbers));
+
+console.assert(numbersOver10(numbers).toString() === "12,18,11,101", {'message': 'numbersOver10 should return "12,18,11,101"'});
+
+
+// 8.
+// Write a function that accepts both an array and number argument
+// and returns an array of only the numbers greater than the number passed to the function
+// A: kenji - coparison operator
+
+let x = 15;
+
+function numbersOverX(array, x) {
+  let overTen = [];
+  for (let i = 0; i < array.length; i++){
+    if ((array[i]) > x){
+        overTen.push(array[i]);
+        }
+    }		return overTen.toString();
+}
+
+console.log(numbersOverX(numbers, x));
+
+console.assert(numbersOverX(numbers, 15).toString() === "18,101", {'message': 'numbersOverX should return "18,101"'});
+
+// 9.
+// Write a function `joinArrays()` that takes an array of arrays,
+// and returns a single array with the contents of the second array
+// listed after the contents of the first array
+var numbers = [1,12,4,18,9,7,11,3,101,5,6];
+var numbersTwo = [33,56,72,2,5,66,90,21,42];
+// A: - kenji - .split and .join
+
+function joinArrays(array1, array2) {
+  let array3 = [];
+  array3 = array1.concat(array2);{
+  }	return array3.toString();
+}
+
+console.log(joinArrays(numbers, numbersTwo));
+
+console.assert(joinArrays([numbers, numbersTwo]).toString() === '1,12,4,18,9,7,11,3,101,5,6,33,56,72,2,5,66,90,21,42', {'message': 'joinArrays should return "1,12,4,18,9,7,11,3,101,5,6,33,56,72,2,5,66,90,21,42"'});
+
+
+// 10.
+// Using the `instructors` array, comment the value of each variable below
+var instructors = [
+    ['JD','JavaScript'],
+    ['Tim','JavaScript'],
+    ['Brit','Ruby'],
+    ['Joe','iOS'],
+    ['Dan','JavaScript'],
+    ['Will','JavaScript'],
+    ['Calvin','JavaScript'],
+    ['James','Ruby']
+];
+
+var instructorNameDiscipline = instructors[5];
+console.log(instructorNameDiscipline);
+// greenvIlleInstructor = <[ 'Will', 'JavaScript' ]>
+
+var instructorOne = instructors[4][0];
+console.log(instructorOne);
+// instructorOne = <Dan>
+
+var instructorTwo = instructors[0][1];
+console.log(instructorTwo);
+// instructorTwo = <JavaScript>
+
+var instructorThree = instructors[2][0];
+console.log(instructorThree);
+// instructorThree = <Brit>
